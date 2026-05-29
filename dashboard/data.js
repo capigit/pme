34d53be6b03d1acf75e2082bb2c0540193f87ec4
balde <alphaamadoub42@gmail.com>
2026-05-29 11:06:48 +0200

    Ajout data.js pour dashboard Netlify

---
+++ dashboard/data.js
@@ -0,0 +1,19 @@
+const reportData = {
+  "chiffre_affaires_total": 5268.78,
+  "par_produit": {
+    "Produit A": 1199.76,
+    "Produit B": 539.73,
+    "Produit C": 449.85,
+    "Produit D": 1679.79,
+    "Produit E": 1399.65
+  },
+  "par_ville": {
+    "Bordeaux": 829.81,
+    "Lille": 249.93,
+    "Lyon": 1059.79,
+    "Marseille": 1009.73,
+    "Nantes": 739.83,
+    "Paris": 799.8,
+    "Strasbourg": 579.89
+  }
+};
